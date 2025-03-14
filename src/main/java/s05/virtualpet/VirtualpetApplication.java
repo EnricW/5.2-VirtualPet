8df544972405026716ea5245294e931bf77c2802
reiben815 <reiben815@gmail.com>
2025-03-14 13:30:36 +0100

    First commit

---
+++ src/main/java/s05/virtualpet/VirtualpetApplication.java
@@ -0,0 +1,13 @@
+package s05.virtualpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s05/virtualpet/VirtualpetApplicationTests.java
@@ -0,0 +1,13 @@
+package s05.virtualpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
