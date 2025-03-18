67b1d6f6608f4ce3ee43375146e716e5c9c8efb5
EnricW <reiben815@gmail.com>
2025-03-18 12:57:14 +0100

    Improve DTO validations

--- src/main/java/s05/virtualpet/dto/PetCreateDTO.java
+++ src/main/java/s05/virtualpet/dto/PetCreateDTO.java
@@ -1,9 +1,13 @@
 package s05.virtualpet.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import s05.virtualpet.enums.PetType;
 
 public record PetCreateDTO(
-        @NotBlank String name,
+        @NotBlank
+        @Size(min = 3, max = 15, message = "Pet name must be between 3 and 15 characters")
+        String name,
+
         PetType type
 ) {}
--- src/main/java/s05/virtualpet/dto/UserRegisterDTO.java
+++ src/main/java/s05/virtualpet/dto/UserRegisterDTO.java
@@ -1,9 +1,19 @@
 package s05.virtualpet.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserRegisterDTO(
-        @NotBlank @Size(min = 4, max = 20) String username,
-        @NotBlank @Size(min = 6, max = 100) String password
+        @NotBlank
+        @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+        String username,
+
+        @NotBlank
+        @Size(min = 8, max = 100, message = "Password must be at least 8 characters long")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+                message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        )
+        String password
 ) {}
